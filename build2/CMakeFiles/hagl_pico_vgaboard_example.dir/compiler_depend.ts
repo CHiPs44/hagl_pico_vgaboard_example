d0f372867a552c12b7290d04af04c34da54bb1c1
CHiPs44 <chips@chips44.fr>
2024-02-20 20:57:21 +0100

    Trying to delete build2...

--- build2/CMakeFiles/hagl_pico_vgaboard_example.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for hagl_pico_vgaboard_example.
--- build2/CMakeFiles/hagl_pico_vgaboard_example.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hagl_pico_vgaboard_example.
-# This may be replaced when dependencies are built.
